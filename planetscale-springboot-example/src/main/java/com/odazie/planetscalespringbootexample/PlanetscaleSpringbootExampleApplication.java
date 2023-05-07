04d0ad497c53133c8a91a7300daf265533007f66
AtulKaushik <kaushikatul033@gmail.com>
2023-05-07 17:54:15 +0530

    initial upload

---
+++ planetscale-springboot-example/src/main/java/com/odazie/planetscalespringbootexample/PlanetscaleSpringbootExampleApplication.java
@@ -0,0 +1,16 @@
+package com.odazie.planetscalespringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@SpringBootApplication
+public class PlanetscaleSpringbootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetscaleSpringbootExampleApplication.class, args);
+    }
+
+}
